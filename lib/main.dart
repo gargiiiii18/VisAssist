279b6fd30e8058bc86b46ccbf2830604eac7a910
gargi <gargi.p.sukhatankar@gmail.com>
2026-01-10 01:04:52 +0530

    fix: update proper importing of dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,11 @@ import 'services/scene_manager.dart';
 import 'widgets/camera_painter.dart';
 
 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await LoggingService().initialize(); // Initialize logging early
   runApp(const VisionApp());
 }
