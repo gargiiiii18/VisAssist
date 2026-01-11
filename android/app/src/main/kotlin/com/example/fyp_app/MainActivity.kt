dfdddeb3be58f838f3be3a76a605215a4488cb6f
gargi <gargi.p.sukhatankar@gmail.com>
2026-01-11 23:36:29 +0530

    chore: update

--- android/app/src/main/kotlin/com/example/fyp_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/fyp_app/MainActivity.kt
@@ -1,5 +1,39 @@
 package com.example.fyp_app
 
+import android.telephony.SmsManager
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+    private val CHANNEL = "com.example.fyp_app/sms"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            call, result ->
+            if (call.method == "sendSMS") {
+                val number = call.argument<String>("number")
+                val message = call.argument<String>("message")
+                if (number != null && message != null) {
+                    sendSMS(number, message, result)
+                } else {
+                    result.error("INVALID_ARGS", "Number or message missing", null)
+                }
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun sendSMS(phoneNumber: String, message: String, result: MethodChannel.Result) {
+        try {
+            val smsManager = SmsManager.getDefault()
+            smsManager.sendTextMessage(phoneNumber, null, message, null, null)
+            result.success("SMS Sent")
+        } catch (e: Exception) {
+            result.error("SMS_FAILED", "Failed to send SMS: ${e.localizedMessage}", null)
+        }
+    }
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import device_info_plus
 import flutter_tts
+import geolocator_apple
 import path_provider_foundation
 import record_macos
 import speech_to_text
@@ -14,6 +15,7 @@ import speech_to_text
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   RecordMacOsPlugin.register(with: registry.registrar(forPlugin: "RecordMacOsPlugin"))
   SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
